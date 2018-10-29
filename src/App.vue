4d5b1907d8d815d7eeb0e5fe7ee10247fce3d6b3
cahyo40 <mdcnugroho@gmail.com>
2018-10-29 22:36:37 +0700

    komit 1

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <home></home>
+  </div>
+</template>
+
+<script>
+import home from './komponen/home.vue'
+export default {
+  name: 'app',
+  components :{
+    home,
+  },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+<style lang="scss">
+@import '../node_modules/bootstrap/scss/bootstrap.scss';
+</style>
---
+++ src/komponen/home.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <h1>Home</h1>
+        <table class="table">
+             <tr v-for="cie in waduh.slice(0,10)" v-bind:key="cie.id">
+                <td >
+                    {{cie.kecamatan}}
+                </td>
+                <td>
+                    {{cie.kelurahan}}
+                </td>
+            </tr>
+        </table>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+       return{
+           waduh :[]
+       }
+    },
+    methods:{
+        getData(){
+            this.$http.get('https://kodepos-2d475.firebaseio.com/kota_kab/k110.json?print=pretty').then(function(response){
+                console.log(response.body);
+                var th = this;
+                th.waduh = response.body;
+            });
+        }
+    },
+    created(){
+        this.getData();
+    }
+}
+</script>
+
